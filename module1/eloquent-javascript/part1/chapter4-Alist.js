cd3157b14e9d0261fdb3167f373fdfbedf834155
pbrodriguezm <pb.rodriguez.m@gmail.com>
2021-02-05 13:58:13 -0500

    chapter4 exercises 3 of 4

--- module1/eloquent-javascript/part1/chapter4-Alist.js
+++ module1/eloquent-javascript/part1/chapter4-Alist.js
@@ -27,7 +27,7 @@ function listToArray(list) {
 
 //console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
-console.log(listToArray(arrayToList([10, 20, 30])));
+console.log(listToArray(arrayToList([10, 20, 30]))); // errorrr missing an element
 // → [10, 20, 30]
 //console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
